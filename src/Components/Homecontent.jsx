e00c47540f97625516d20404fbd291089bf1651a
aparnarajan-m <aparna.meleveettil@gmail.com>
2024-11-02 22:14:28 +0530

    sliderwork

--- src/Components/Homecontent.jsx
+++ src/Components/Homecontent.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import './Homecontent.css'
+import dashboardbanner1 from '/src/assets/Dashboards_Banner_bg.png' 
 
 function Homecontent() {
   return (
-    <div className='home-container'>
-      <div className='top1'><h5>Starbucks</h5></div> 
-      <div className='top2'><button className='homebutton1'>Know More</button></div>  
+    <div>
+      <div className='home-container'>
+        <div className='top1'><h5>Starbucks</h5></div>
+        <div className='top2'><button className='homebutton1'>Know More</button></div>
+      </div>
+      <div className='homeslider-div'>
+          <div><img className='homeslider' src={dashboardbanner1} alt="" /></div>
+      </div>
     </div>
+
   )
 }
 
